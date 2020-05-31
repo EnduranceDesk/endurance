46a34c8e9bdf7bda1533f205d0474d753c86654c
Adnan <adnanhussainturki@gmail.com>
2020-05-31 19:19:17 +0530

    Minor Fix

---
+++ app/Console/Commands/RestrictedCommands/Make/Migration.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands\RestrictedCommands\Make;
+
+use Illuminate\Console\Command;
+
+class Migration extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:migration {path}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->error("Endurance cannot create migrations. Use endeavour.");
+    }
+}
