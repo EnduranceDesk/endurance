e79b06f4ecb474b18d86cec1952f00179fb05d6c
Adnan <adnanhussainturki@gmail.com>
2022-03-05 01:58:46 +0530

    Added Misc

--- app/Helpers/Misc.php
+++ app/Helpers/Misc.php
@@ -4,7 +4,7 @@ use Zip;
 
 class Misc {
     public static function  makeZipWithFiles(string $zipPathAndName, array $filesAndPaths) {
-        $zip = Zip::open($zipPathAndName);
+        $zip = Zip::create($zipPathAndName);
         $zip->add($filesAndPaths);
         $zip->close();
     }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -73,6 +73,7 @@
         </nav>
 
         <main class="py-4">
+            @include("layouts.partials.alert")
             @yield('content')
         </main>
     </div>
