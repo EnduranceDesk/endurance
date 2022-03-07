cda70c76697ee4d8d3b35f3c0131f3716aa94c3e
Adnan <adnanhussainturki@gmail.com>
2022-03-08 00:48:32 +0530

    Fix

--- routes/partials/rover_route.php
+++ routes/partials/rover_route.php
@@ -2,7 +2,7 @@
 
 Route::get('/rover/list', 'Rover\RoverController@getList')->name('rover.list');
 Route::get('/rover/{username}/destroy', 'Rover\RoverController@getDestroy')->name('rover.destroy');
-Route::get('/rover/{rover}/ssl/auto', 'Rover\RoverController@getDestroy')->name('rover.ssl.auto');
+Route::get('/rover/{domain}/ssl/auto', 'Rover\RoverController@getDestroy')->name('rover.ssl.auto');
 Route::get('/rover/builder', 'Rover\RoverController@getBuilder')->name('rover.builder');
 Route::post('/rover/builder', 'Rover\RoverController@postBuilder')->name('rover.build');
 Route::post('/rover/php/version/change', 'Rover\RoverController@postChangePHPVersion')->name('rover.php.change');
